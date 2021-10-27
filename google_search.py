6d2b1a9cd23405754f45c9a33c69f5de25fe124b
Sharun E Rajeev <sharunpublic@gmail.com>
2021-10-27 11:50:04 +0530

    update google_search

--- google_search.py
+++ google_search.py
@@ -4,11 +4,13 @@
 # import webdriver
 from selenium import webdriver
   
+question = input("Enter Question to be searched - ")
+
 # create webdriver object
 driver = webdriver.Chrome()
 
-question = input("Enter Question to be searched")
 url = "https://google.co.in/search?q=" + question
+print(url)
   
 # get google.co.in
 driver.get(url)
